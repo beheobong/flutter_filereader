f6607b63ee25835c65659264a41a87647cea6740
famtechdu <dovandu9x03@gmail.com>
2020-07-03 14:52:13 +0700

    remove android

--- lib/flutter_filereader.dart
+++ lib/flutter_filereader.dart
@@ -78,28 +78,26 @@ class _FileReaderViewState extends State<FileReaderView> {
         return _loadingWidget();
       }
     } else {
-      return Center(child: Text("不支持的平台"));
+      return Center(child: Text("Nền tảng không được hỗ trợ"));
     }
   }
 
   Widget _unSupportFile() {
     return widget.unSupportFileWidget ??
         Center(
-          child: Text("不支持打开${_fileType(filePath)}类型的文件"),
+          child: Text("Không hỗ trợ mở${_fileType(filePath)} loại file"),
         );
   }
 
   Widget _fileNotFoundFile() {
     return Center(
-      child: Text("文件不存在"),
+      child: Text("Tập tin không tồn tại"),
     );
   }
 
   Widget _enginLoadFail() {
-    //最有可能是abi的问题,x5不支持64位的arm架构,所以需要abi过滤为armeabi 或者armv7a
-    //还有可能第一次下载成功,但是加载不成功
     return Center(
-      child: Text("引擎加载失败,请退出重试"),
+      child: Text("Không tải được, vui lòng thoát ra và thử lại"),
     );
   }
 
